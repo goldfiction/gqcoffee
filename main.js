606ffa6f3cfa2457728d1729606425a7e3299f59
Your Name <glidev5@gmail.com>
2017-03-23 17:12:55 -0400

    update

---
+++ main.js
@@ -0,0 +1,42 @@
+/**
+ * Created by happy on 3/23/17.
+ */
+var CoffeeScript = require('coffee-script');
+var fs = require( 'fs' );
+var path=require('path');
+var fsReaddir = require('fs-readdir');
+var async=require('async');
+
+function load(o,cb){
+    var path2= o.path||"coffee";
+    var coffees={};
+    console.log('loading coffee scripts from path: '+path2);
+    fsReaddir(path2,function(e,files){
+        if( e ) {
+            console.log(e.stack);
+            cb(e);
+        }else{
+            async.each(files,function(file,cb){
+                console.log(file)
+                var pathnew=file;
+                var filename=file.toLowerCase().replace('.coffee',"");
+                if (file.indexOf('.coffee') != -1) {
+                    try {
+                        coffees[filename] = (CoffeeScript.compile(fs.readFileSync(pathnew).toString())) || "";
+                        cb();
+                    } catch (e) {
+                        console.log(e.stack);
+                        cb(e);
+                    }
+                }
+            },function(e){
+                if(e){
+                    console.log(e.stack);
+                }
+                cb(e,coffees);
+            })
+        }
+    })
+}
+
+exports.load=load;
---
+++ test/test.js
@@ -0,0 +1,20 @@
+/**
+ * Created by happy on 3/23/17.
+ */
+
+var assert=require('assert');
+var gqcoffee=require('../main.js');
+
+
+it('should be able to load coffees',function(done){
+    gqcoffee.load({path:"test/coffee"},function(e,coffee){
+        global.coffee=coffee;
+        console.log("global.coffee");
+        console.log(JSON.stringify(coffee,null,2));
+        console.log('executing b');
+        eval(coffee['test/coffee/sub/b']);
+        console.log('b='+b);
+        assert(b==456);
+        done(e);
+    });
+})
